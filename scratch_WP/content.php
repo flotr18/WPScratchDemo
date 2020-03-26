fcb66689b3b5a7478fc11a3ec260511274fcc7f0
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-03-26 10:43:56 +0100

    Divided index sections

---
+++ scratch_WP/content.php
@@ -0,0 +1,3 @@
+<div class="title">
+    <h1>Hello There ! </h1>
+</div>
---
+++ scratch_WP/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en"
+      <head>
+          <title>WP Theme from Scratch</title>
+          <?php wp_head(); ?>
+      </head>
+<body>
+<div class="container">
+
